d893443277bf558cb9c8acd4c9a46f5d1d98e198
Roger Earl <rogerearl@me.com>
2022-11-09 20:34:11 -0800

    added game mechanics

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 class Casino {
-    // Write code here
+    constructor(name, cheater) {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.isFakeCoin = cheater
+    }
+
+    playGame(betAmount) {
+        this.timesPlayed += 1;
+
+        let randomNum = Math.random();
+        if (this.isFakeCoin || randomNum < .5) {
+            // player loses
+            console.log(`${this.name} Wins`)
+        }else {
+            // player wins
+            let winnings = betAmount * (this.timesPlayed + 1)
+            console.log(`You win ${winnings}`)
+        }
+    }
 };
 
 // TESTS
@@ -11,20 +29,20 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
-const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
-console.log(myExtraBonusCasino);
-myExtraBonusCasino.rollDie(6);
-myExtraBonusCasino.rollDie(20);
-myExtraBonusCasino.rollDie(100);
-*/
+
+// const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
+// console.log(myExtraBonusCasino);
+// myExtraBonusCasino.rollDie(6);
+// myExtraBonusCasino.rollDie(20);
+// myExtraBonusCasino.rollDie(100);
+
